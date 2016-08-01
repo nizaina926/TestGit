aed9e86aa79702c97e1c86e80736c74b0800aad9
nizaina926 <1092377615@qq.com>
2016-08-02 01:03:22 +0800

    ！
    ！

--- src/com/atguigu/git/test/Test01.java
+++ src/com/atguigu/git/test/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("~~~~~~~~~~~~~~~~~");
 		System.out.println("*****************");
+		System.out.println("！！！！！！！！！");
 	}
 
 }
